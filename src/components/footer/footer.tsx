0bc0e0a8f91d50f4316411dcebaa13560a8472c5
tommy-josepovic <tommy-josepovic@users.noreply.github.com>
2020-12-23 12:36:27 -0500

    Adjustment to footer. Added favicon.

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -6,9 +6,9 @@ function Footer() {
     <footer className="footer">
       <h3>Shoppies v2</h3>
       <p>
-        Made by <a href="https://github.com/tommy-josepovic">Tommy Josepovic</a>. <br />
-        <a href="https://github.com/tommy-josepovic/shoppies-2">View the code</a>. <br />
-        <a href="https://shoppies-challenge.web.app/">View Shoppies v1</a>.
+        Made by <a href="https://github.com/tommy-josepovic" target="_blank" rel="noreferrer" >Tommy Josepovic</a>. <br />
+        <a href="https://github.com/tommy-josepovic/shoppies-2" target="_blank" rel="noreferrer">View the code</a>. <br />
+        <a href="https://shoppies-challenge.web.app/" target="_blank" rel="noreferrer">View Shoppies v1</a>.
       </p>
     </footer>
   )
