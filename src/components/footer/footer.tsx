d2782d360e8e6c48da658070281526fbdf8e39f3
tommy-josepovic <tommy-josepovic@users.noreply.github.com>
2020-12-23 12:54:16 -0500

    Slightly adjusted spacing.

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -6,11 +6,15 @@ function Footer() {
     <footer className="footer">
       <h3>Shoppies v2</h3>
       <p>
-        Made by <a href="https://github.com/tommy-josepovic" target="_blank" rel="noreferrer" >Tommy Josepovic</a>. <br />
-        <a href="https://github.com/tommy-josepovic/shoppies-2" target="_blank" rel="noreferrer">View the code</a>. <br />
+        Made by <a href="https://github.com/tommy-josepovic" target="_blank" rel="noreferrer" >Tommy Josepovic</a>.
+        </p>
+      <p>
+        <a href="https://github.com/tommy-josepovic/shoppies-2" target="_blank" rel="noreferrer">View the code</a>.
+        </p>
+      <p>
         <a href="https://shoppies-challenge.web.app/" target="_blank" rel="noreferrer">View Shoppies v1</a>.
       </p>
-    </footer>
+    </footer >
   )
 }
 
